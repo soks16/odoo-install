f63b6be425bb9d175f85de3438037346a5d23b89
Jesus sokamby <59472235+soks16@users.noreply.github.com>
2024-10-20 07:19:33 +0100

    Update start_docker_odoo.sh

--- start_docker_odoo.sh
+++ start_docker_odoo.sh
@@ -62,16 +62,6 @@ while [[ -z "$PROJECT_NAME" ]]; do
   fi
 done
 
-# Demander si l'utilisateur veut installer deux instances
-while true; do
-  read -p "Combien d'instances voulez-vous installer ? (1 = TEST seulement, 2 = PROD et TEST) : " INSTALL_TWO_INSTANCES
-  case $INSTALL_TWO_INSTANCES in
-    1) INSTALL_TWO_INSTANCES="no"; break;;
-    2) INSTALL_TWO_INSTANCES="yes"; break;;
-    *) echo "Veuillez répondre par 1 ou 2.";;
-  esac
-done
-
 
 # Définir les chemins de configuration et de clonage
 PROD_CONFIG="/opt/odoo${OE_VERSION}/config/${PROJECT_NAME}"
@@ -122,12 +112,10 @@ function is_port_in_use() {
   nc -z localhost "$1" 2>/dev/null
 }
 
-CUSTOM_PORT_T=2
-CUSTOM_PORT_P=1
+CUSTOM_PORT=2
 
 # Set initial ports
-TEST_PORT=${CUSTOM_PORT_T}00${OE_VERSION}
-PROD_PORT=${CUSTOM_PORT_P}00${OE_VERSION}
+INSTANCE_PORT=${CUSTOM_PORT}00${OE_VERSION}
 
 function find_available_port() {
   local base_port=$1
@@ -148,8 +136,7 @@ db_user = odoo
 db_password = ${PASS_DB}
 ;addons_path = /mnt/extra-addons/theme,/mnt/extra-addons/custom,/mnt/extra-addons/OCA/web
 addons_path = /mnt/enterprise,/mnt/extra-addons/custom
-;dbfilter = ^${PROJECT_NAME}_V${OE_VERSION}_PROD$
-;dbfilter = ^${PROJECT_NAME}_V${OE_VERSION}_TEST$
+;dbfilter = ^DB_${PROJECT_NAME}_V${OE_VERSION}$
 logfile = /var/log/odoo/odoo-server.log
 log_db = True
 workers = 4
@@ -157,128 +144,25 @@ workers = 4
 ; WebSocket settings
 ;evented = True
 ;evented_port = 8072
-;xmlrpc_port = $(find_available_port $TEST_PORT)
-;xmlrpc_port = $(find_available_port $PROD_PORT)
+;xmlrpc_port = $(find_available_port $INSTANCE_PORT)
 ;session_gc = True
 ;session_timeout = 7200
 ;session_cookie_samesite = None 
-;session_store = redis
-;redis_url = redis://<redis_host>:<redis_port>
 EOL
 echo "Le fichier odoo.conf a été créé."
-
-# Si l'utilisateur choisit d'installer deux instances
-if [ "$INSTALL_TWO_INSTANCES" = "yes" ]; then
-  # Créer les répertoires de configuration pour production et test
-  # Définir les bonnes permissions (optionnel)
-  PROD_PORT=$(find_available_port $PROD_PORT)
-  TEST_PORT=$(find_available_port $TEST_PORT)
-  sudo mkdir -p $TEST_CONFIG 
-  sudo mkdir -p $PROD_CONFIG
-
-  sudo touch /opt/odoo${OE_VERSION}/odoo-log-test/${PROJECT_NAME}/odoo-server.log
-
-  # Copier le fichier de configuration vers les répertoires respectifs
-  echo "Copie du fichier odoo.conf vers /opt/odoo${OE_VERSION}/config/${PROJECT_NAME} pour la production..."
-  sudo cp $SRC_CONFIG $PROD_CONFIG/odoo.conf && echo "Copie vers la configuration de production réussie."
-  sudo chmod 644 $PROD_CONFIG/odoo.conf
+INSTANCE_PORT=$(find_available_port $INSTANCE_PORT)
   
-  echo "Copie du fichier odoo.conf vers /opt/odoo${OE_VERSION}/config-test/${PROJECT_NAME} pour le test..."
-  sudo mv $SRC_CONFIG $TEST_CONFIG/odoo.conf && echo "Copie vers la configuration de test réussie."
-  sudo chmod 644 $TEST_CONFIG/odoo.conf
-  sudo chmod 755 -R $PROD_CONFIG
-  sudo chmod 755 -R $TEST_CONFIG
+# Si l'utilisateur choisit d'installer uniquement une instance de test
+sudo mkdir -p $TEST_CONFIG
+sudo chmod 755 -R $TEST_CONFIG
 
-  # Créer docker-compose.yml avec odoo_prod et odoo_test
-  sudo tee $COMPOSE_FILE > /dev/null <<EOL
-version: '3'
-
-services:
-  postgres:
-    image: postgres:$POSTGRES_VERSION
-    container_name: ${PROJECT_NAME}_V${OE_VERSION}_db
-    environment:
-      - POSTGRES_USER=odoo
-      - POSTGRES_PASSWORD=${PASS_DB}
-      - POSTGRES_DB=postgres
-    volumes:
-      - /opt/odoo${OE_VERSION}/odoo-db-data/${PROJECT_NAME}:/var/lib/postgresql/data
-    networks:
-      - odoo-network
-    restart: always
-
-  odoo_prod:
-    image: odoo:${GIT_VERSION}
-    container_name: ${PROJECT_NAME}_V${OE_VERSION}_prod
-    depends_on:
-      - postgres
-    tty: true
-    environment:
-      - HOST=${PROJECT_NAME}_V${OE_VERSION}_db
-      - USER=odoo
-      - PASSWORD=${PASS_DB}
-    volumes:
-      - /opt/odoo${OE_VERSION}/custom/addons/${PROJECT_NAME}:/mnt/extra-addons/custom
-      - /opt/odoo${OE_VERSION}/OCA/brand:/mnt/extra-addons/OCA/brand
-      - /opt/odoo${OE_VERSION}/OCA/server-tools:/mnt/extra-addons/OCA/server-tools
-      - /opt/odoo${OE_VERSION}/OCA/web:/mnt/extra-addons/OCA/web
-      - /opt/odoo${OE_VERSION}/enterprise/addons:/mnt/enterprise
-      - /opt/odoo${OE_VERSION}/config/${PROJECT_NAME}:/etc/odoo
-      - /opt/odoo${OE_VERSION}/odoo-log/${PROJECT_NAME}:/var/log/odoo
-      - /opt/odoo${OE_VERSION}/odoo-web-data/${PROJECT_NAME}:/var/lib/odoo
-      - "/opt/odoo${OE_VERSION}/odoo-web-data/${PROJECT_NAME}:/var/lib/odoo"
-    ports:
-      - "${PROD_PORT}:8069"
-    networks:
-      - odoo-network
-    restart: always
-
-  odoo_test:
-    image: odoo:${GIT_VERSION}
-    container_name: ${PROJECT_NAME}_V${OE_VERSION}_test
-    depends_on:
-      - postgres
-    tty: true
-    environment:
-      - HOST=${PROJECT_NAME}_V${OE_VERSION}_db
-      - USER=odoo
-      - PASSWORD=${PASS_DB}
-    volumes:
-      - /opt/odoo${OE_VERSION}/custom/addons/${PROJECT_NAME}:/mnt/extra-addons/custom
-      - /opt/odoo${OE_VERSION}/OCA/brand:/mnt/extra-addons/OCA/brand
-      - /opt/odoo${OE_VERSION}/OCA/server-tools:/mnt/extra-addons/OCA/server-tools
-      - /opt/odoo${OE_VERSION}/OCA/web:/mnt/extra-addons/OCA/web
-      - /opt/odoo${OE_VERSION}/enterprise/addons:/mnt/enterprise
-      - /opt/odoo${OE_VERSION}/config-test/${PROJECT_NAME}:/etc/odoo
-      - /opt/odoo${OE_VERSION}/odoo-log-test/${PROJECT_NAME}:/var/log/odoo
-      - "/opt/odoo${OE_VERSION}/odoo-web-data/${PROJECT_NAME}:/var/lib/odoo"
-    ports:
-      - "${TEST_PORT}:8069"
-    networks:
-      - odoo-network
-    restart: always
-    
-volumes:
-  - odoo-db-data-${PROJECT_NAME}:/var/lib/postgresql/data
-  - odoo-web-data-${PROJECT_NAME}:/var/lib/odoo
-
-networks:
-  odoo-network:
-EOL
+# Copier le fichier de configuration uniquement pour le test
+echo "Copie du fichier odoo.conf vers /opt/odoo${OE_VERSION}/config-test/${PROJECT_NAME} pour le test..."
+sudo mv $SRC_CONFIG $TEST_CONFIG/odoo.conf && echo "Copie vers la configuration de test réussie."
+sudo chmod 644 $TEST_CONFIG/odoo.conf
   
-else
-
-  TEST_PORT=$(find_available_port $TEST_PORT)
-  # Si l'utilisateur choisit d'installer uniquement une instance de test
-  sudo mkdir -p $TEST_CONFIG
-  sudo chmod 755 -R $TEST_CONFIG
-  # Copier le fichier de configuration uniquement pour le test
-  echo "Copie du fichier odoo.conf vers /opt/odoo${OE_VERSION}/config-test/${PROJECT_NAME} pour le test..."
-  sudo mv $SRC_CONFIG $TEST_CONFIG/odoo.conf && echo "Copie vers la configuration de test réussie."
-  sudo chmod 644 $TEST_CONFIG/odoo.conf
-  
-  # Créer docker-compose.yml avec seulement odoo_test
-  sudo tee $COMPOSE_FILE > /dev/null <<EOL
+# Créer docker-compose.yml avec seulement odoo_test
+sudo tee $COMPOSE_FILE > /dev/null <<EOL
 version: '3'
 
 services:
@@ -316,7 +200,7 @@ services:
       - /opt/odoo${OE_VERSION}/odoo-log-test/${PROJECT_NAME}:/var/log/odoo
       - "/opt/odoo${OE_VERSION}/odoo-web-data/${PROJECT_NAME}:/var/lib/odoo"
     ports:
-      - "${TEST_PORT}:8069"
+      - "${INSTANCE_PORT}:8069"
     networks:
       - odoo-network
     restart: always
@@ -325,35 +209,22 @@ networks:
   odoo-network:
 EOL
 
-fi
-
-# Lancer Docker Compose avec sudo et le nom du projet
 echo "Démarrage de Docker Compose pour Odoo version ${OE_VERSION} avec le projet '${PROJECT_NAME}'..."
 sudo docker-compose -f docker-compose.yml -p ${PROJECT_NAME}_V${OE_VERSION} up -d
 sudo chmod 755 -R /opt/odoo${OE_VERSION}/custom/addons/${PROJECT_NAME}
 sudo mv $COMPOSE_FILE /opt/odoo${OE_VERSION}/custom/addons/${PROJECT_NAME}/docker-compose.yml
 sudo chmod 777 /opt/odoo${OE_VERSION}/custom/addons/${PROJECT_NAME}/docker-compose.yml
 
-# Afficher l'adresse IP de la machine physique et les ports
-IP_ADDRESS=$(hostname -I | awk '{print $1}')  # Get the first IP address
-echo "Les instances Odoo version ${GIT_VERSION} sont maintenant en cours d'exécution."
-echo "Ports utilisés :"
-
-if [ "$INSTALL_TWO_INSTANCES" = "yes" ]; then
+if [ $? -eq 0 ]; then
+  echo "Les instances Odoo version ${GIT_VERSION} sont maintenant en cours d'exécution."
+  IP_ADDRESS=$(hostname -I | awk '{print $1}')  # Récupérer la première adresse IP
+  echo "Ports utilisés :"
   sudo chmod 777 -R /opt/odoo${OE_VERSION}
-  echo "  - Production: http://$IP_ADDRESS:$PROD_PORT"
-  echo "  - Test: http://$IP_ADDRESS:$TEST_PORT"
-  echo "Pour des modifications sur les containers veuillez modifier le fichier docker-compose.yml et excuter la commande suivante:"
+  echo "  - Intance crée: http://$IP_ADDRESS:$INSTANCE_PORT"
+  echo "Pour des modifications sur les containers veuillez modifier le fichier docker-compose.yml et exécuter la commande suivante :"
   echo "==> Chemin: cd /opt/odoo${OE_VERSION}/custom/addons/${PROJECT_NAME}"
-  echo "==> docker-compose -f docker-compose.yml -p ${PROJECT_NAME}_V${OE_VERSION} up -d"
-  echo "==> File conf: nano /opt/odoo${OE_VERSION}/config/${PROJECT_NAME}/odoo.conf"
-  echo "==> File conf: nano /opt/odoo${OE_VERSION}/config-test/${PROJECT_NAME}/odoo.conf"
+  echo "==> Commande: docker-compose -f docker-compose.yml -p ${PROJECT_NAME}_V${OE_VERSION} up -d"
+  echo "==> Fichier de configuration: nano /opt/odoo${OE_VERSION}/config-test/${PROJECT_NAME}/odoo.conf"
 else
-  sudo chmod 777 -R /opt/odoo${OE_VERSION}
-  echo "  - Test: http://$IP_ADDRESS:$TEST_PORT"
-  echo "Pour des modifications sur les containers veuillez modifier le fichier docker-compose.yml et excuter la commande suivante:"
-  echo "==> Chemin: cd /opt/odoo${OE_VERSION}/custom/addons/${PROJECT_NAME}"
-  echo "==> Command: docker-compose -f docker-compose.yml -p ${PROJECT_NAME}_V${OE_VERSION} up -d"
-  echo "==> File conf: nano /opt/odoo${OE_VERSION}/config-test/${PROJECT_NAME}/odoo.conf"
+  echo "Erreur lors de l'exécution de Docker Compose. Veuillez vérifier la configuration et réessayer."
 fi
-
